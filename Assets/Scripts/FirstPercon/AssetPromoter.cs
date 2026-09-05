2d830076dbe11e5653355908221c331be6f1915a
Temrik-web <lokiibobgo@gmail.com>
2026-09-05 19:46:02 +0300

    Подготовить основную сцену и редакторские скрипты к сборке

--- Assets/Scripts/FirstPercon/AssetPromoter.cs
+++ Assets/Scripts/FirstPercon/AssetPromoter.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 
@@ -123,3 +124,4 @@ namespace EasyPeasyFirstPersonController.EditorScripts
         }
     }
 }
+#endif
--- Assets/Scripts/FirstPercon/ReviewWindowEditor.cs
+++ Assets/Scripts/FirstPercon/ReviewWindowEditor.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using EasyPeasyFirstPersonController;
 using UnityEditor;
 using UnityEngine;
@@ -44,3 +45,4 @@ namespace EasyPeasyFirstPersonController.EditorScripts
         }
     }
 }
+#endif
